6d6fdfd98e8cfa0c60d50a35776e880b6630da11
simoneburrai <burrai.s98@gmail.com>
2025-04-10 13:56:26 +0200

    created select html

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,5 +1,39 @@
+import { useEffect } from "react";
+import { useState } from "react";
+
 function Main({ movies }) {
-    return <main>Sono il Main</main>
+
+    const [selectCategory, setSelectCategory] = useState("");
+
+
+    useEffect(() => {
+        const selectedMovies = movies;
+        if (selectCategory !== "") {
+
+        }
+
+
+    }, [selectCategory])
+
+    return <main>
+        <div className="select-container">
+            <select onChange={(e) => setSelectCategory(e.target.value)}>
+                <option value="">Categories</option>
+                <option value={selectCategory}>Fantascienza</option>
+                <option value={selectCategory}>Thriller</option>
+                <option value={selectCategory}>Azione</option>
+                <option value={selectCategory}>Romantico</option>
+            </select>
+        </div>
+        <div className="movies-container">
+            {movies.map((movie, i) => {
+                return <div key={i} className="movie">
+                    <h2>{movie.title}</h2>
+                    <h3>{movie.genre}</h3>
+                </div>
+            })}
+        </div>
+    </main>
 }
 
 export default Main;
